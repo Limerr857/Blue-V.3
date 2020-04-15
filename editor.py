bcec991da79016c5ef5e6b8b77432cbb17743d98
Limerr857 <georgelgeback@gmail.com>
2020-04-15 19:13:29 +0200

    Made initial changes

--- editor.py
+++ editor.py
@@ -13,25 +13,48 @@ win = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
 pygame.display.set_caption("Blue V.3 Editor")
 clock = pygame.time.Clock()
 
+menu_page = 1
+
 object_list = [
 
               "img/cobble.png"
 
               ]
 
-class _object():
+current_map = []
+
+class _object(pygame.sprite.Sprite):
 
     def __init__(self, type, location):
         self.type = type
         self.location = location
 
         if type == 0:
-            pass
+            # Cobblestone
+            Cobblestone.__init__(self)
+
+    def setup(self):
+        self.size = self.image.get_rect().size
+        self.mask = pygame.mask.from_surface(self.image)
+
+class Cobblestone(_object):
+    def __init__(self):
+        self.image = img.load(object_list[0]).convert_alpha()
+        self.setup()
+        self.rect = self.image.get_rect()
 
 
 def updates_and_draw():
     
-    if 
+    # Update and draw menu
+    if menu_page == 1:
+        import os
+        print(os.getcwd())
+        cbbl = img.load("cobble.png").convert()
+        win.blit(cbbl, (10, 10))
+        
+
+   
 
 
 run = True
@@ -42,6 +65,10 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    
+    # TODO: replace with actual menu
+    if keys[pygame.K_ESCAPE]:
+        run = False
 
     updates_and_draw()
     pygame.display.update()
